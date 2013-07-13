26d7a1737a226d84019bdf46a51bd42de3448e1b
Nick DeNardis <nick.denardis@gmail.com>
2013-07-13 16:07:23 -0400

    Changed the ex1 text to a todo example

--- class1/exercise1/javascript.js
+++ class1/exercise1/javascript.js
@@ -4,4 +4,4 @@ var perDay = 2;
 
 var days = (oldAge - age) * 365;
 var total = perDay * days;
-alert("You will need " + total + " to last you until the ripe old age of " + oldAge);
+console.log("You have " + total + " tasks left until the ripe old age of " + oldAge);
