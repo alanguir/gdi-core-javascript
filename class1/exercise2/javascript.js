b272487e13d06c9369c46ce3b5f857a95f439473
Nick DeNardis <nick.denardis@gmail.com>
2013-07-13 16:23:20 -0400

    Changed ex2 & ex3 to build off ex1.

--- class1/exercise2/javascript.js
+++ class1/exercise2/javascript.js
@@ -1,11 +1,13 @@
-var age = 26;
-var oldAge = 96;
-var perDay = 2;
-
-var days = (oldAge - age) * 356;
-var total = perDay * days;
-if(total > 40000){
-    alert("You will need " + total + " to last you until the ripe old age of " + oldAge + ". Wow! That's a lot!");
-}else{
-    alert("You will need " + total + " to last you until the ripe old age of " + oldAge + ". You seem pretty reasonable");
+function calculate(){
+	var age = 26;
+	var oldAge = 96;
+	var perDay = 2;
+	
+	var days = (oldAge - age) * 365;
+	var total = perDay * days;
+	if(total > 40000){
+		console.log("You have " + total + " tasks left until the ripe old age of " + oldAge + ". Wow! That's a lot!");
+	}else{
+		console.log("You have " + total + " tasks left until the ripe old age of " + oldAge + ". You better get started!");
+	}
 }
--- class1/exercise3/javascript.js
+++ class1/exercise3/javascript.js
@@ -6,8 +6,8 @@ function calculate(){
     var days = (oldAge - age) * 356;
     var total = perDay * days;
     if(total > 40000){
-        alert("You will need " + total + " to last you until the ripe old age of " + oldAge + ". Wow! That's a lot!");
-    }else{
-        alert("You will need " + total + " to last you until the ripe old age of " + oldAge + ". You seem pretty reasonable");
-    } 
+		console.log("You have " + total + " tasks left until the ripe old age of " + oldAge + ". Wow! That's a lot!");
+	}else{
+		console.log("You have " + total + " tasks left until the ripe old age of " + oldAge + ". You better get started!");
+	}
 }
