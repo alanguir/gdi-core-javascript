f6c3fedca9fc8b1a65e0fd819d04f4dbbb59bdd3
Nick DeNardis <nick.denardis@gmail.com>
2013-07-17 16:23:34 -0400

    Updated the class 3 and 4 to include the newest jquery from Google.

--- class1/exercise2/javascript.js
+++ class1/exercise2/javascript.js
@@ -1,13 +1,14 @@
-function calculate(){
+//function calculate(){
 	var age = 26;
 	var oldAge = 96;
 	var perDay = 2;
 	
 	var days = (oldAge - age) * 365;
 	var total = perDay * days;
+	
 	if(total > 40000){
 		console.log("You have " + total + " tasks left until the ripe old age of " + oldAge + ". Wow! That's a lot!");
 	}else{
 		console.log("You have " + total + " tasks left until the ripe old age of " + oldAge + ". You better get started!");
 	}
-}
+//}
