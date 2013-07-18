2a821856487a5f2b21cb62408a5de90ccd41a97d
Nick DeNardis <nick.denardis@gmail.com>
2013-07-17 21:56:21 -0400

    Rewote class 4 example 2

--- class4/exercise2/javascript.js
+++ class4/exercise2/javascript.js
@@ -1,51 +1,21 @@
 $(document).ready(function(){
+  $('#my-events').submit(function(){
     getMeetups();
-    $('.box').bind({
-        click: function() {
-          $(this).css('background-color', 'green')
-          $(this).html('Clicked!')
-        },
-        mouseenter: function() {
-          $(this).css('background-color', 'purple')
-          $(this).html('Hi!')
-        },
-        mouseleave: function(){
-          $(this).css('background-color', 'orange')
-          $(this).html('Bye!')
-        }
-    });
-    $('#calculate').submit(function(event){
-        var givenAge = $('#age').val();
-        var givenSnack = $('#snack').val();
-        var givenPerDay = $('#times-per-day').val();
-        $('#lifetime-supply').html(calculate(givenAge, givenSnack, givenPerDay));
-        return false;
-    });
-    $('#favorites').submit(function(event){
-        var givenThing = $('#thing').val();
-        favoriteThings(givenThing);
-        return false
-    });
-    $('#friends').submit(function(event){
-       var name = $('#friend-name').val();
-       var hair = $('#friend-hair').val()
-       var friend = {name: name, hair:hair};
-        myFriends(friend);
-        return false;
-    });
+
+    return false;
+  });
 });
 
 function getMeetups(){
 	var api_key = "50722e1d56c194e61763a2ee1e4535";
 	var url = "https://api.meetup.com/2/";
-	var method = "open_events"
+	var method = "open_events";
 	$.ajax({
 		url: url + method,
 		data: {
 		  key: api_key,
-		  lat: 40.7143528,
-		  lon: -74.0059731,
-		  topic: 'JavaScript'
+		  zip: $('#zip').val(),
+		  topic: $('#topic').val()
 		},
 		crossDomain: true,
 		dataType: 'jsonp',
