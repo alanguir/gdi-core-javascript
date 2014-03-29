ccf8fe391ddfc7d4460341d0b1ee4312aef0c8b2
Alan Languirand <alan@13protons.com>
2014-03-29 00:44:03 -0400

    making the class better

--- class1/exercise1/javascript.js
+++ class1/exercise1/javascript.js
@@ -3,5 +3,10 @@ var oldAge = 96;
 var perDay = 2;
 
 var days = (oldAge - age) * 365;
-var total = perDay * days;
-console.log("You have " + total + " tasks left until the ripe old age of " + oldAge);
+
+/* calculate the total and then show it in the log */
+
+//var total = ?
+
+
+
